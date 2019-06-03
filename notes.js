11022f5d519f87fe4d87cdcc99ef03df39b4f288
cjiava251 <zlivkosyava@gmail.com>
2019-06-03 16:06:43 +0300

    added simple login

--- notes.js
+++ notes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
+const passport=require('passport');
+const localStrategy=require('passport-local').Strategy;
 const sqlite3 = require('sqlite3').verbose();
+const redis=require('redis');
 const port = 3000;
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 const db = new sqlite3.Database('./db/users_db.sqlite3');
 db.serialize(() => {
-  db.run('CREATE TABLE IF NOT EXISTS users (userName char(20), familyName char(20), name char(20), patronymic char(20), birthday date, email char(20), mobileNumber char(11), notesCount tinyint)');
+  db.run('CREATE TABLE IF NOT EXISTS users (userName char(20), password char(20), familyName char(20), name char(20), patronymic char(20), birthday date, email char(20), mobileNumber char(11), notesCount tinyint)');
   db.run('CREATE TABLE IF NOT EXISTS notes (noteName text, noteText text, user char(20), tags char(20), likes tinyint, tagsCount tinyint)');
 });
 
@@ -14,30 +17,44 @@ let options={
   index: 'main.html',
   redirect: false
 }
-
+/*
+passport.use(new localStrategy(
+  (userName,password,done) => {
+    findUser(userName,(err,user) => {
+      if (err) return done(err);
+      if (!user) return done(null,false);
+      if (password!==user.password) return done(null,false);
+      return done(null,user);
+    });
+  }
+));
+*/
 app.use('/css', express.static('static'));
 app.use('/', express.static('html',options));
 
 app.listen(port, () => console.log('example app listening on port ' + port));
 
 app.post('/new_user.html', urlencodedParser, (req, res) => {
-  if (req.body.userName && req.body.familyName && req.body.name && req.body.email) {
-    db.run('INSERT INTO users VALUES (?,?,?,?,?,?,?,?)', req.body.userName, req.body.familyName, req.body.name, req.body.patronymic, req.body.birthday, req.body.email, req.body.mobileNumber, 0);
+    db.run('INSERT INTO users VALUES (?,?,?,?,?,?,?,?,?)', req.body.userName, req.body.password, req.body.familyName, req.body.name, req.body.patronymic, req.body.birthday, req.body.email, req.body.mobileNumber, 0);
     res.send('Пользователь ' + req.body.userName + ' успешно зарегистрирован<br><a href="/">на главную</a>');
-  }
-  else res.redirect('back');
 });
 
 app.post('/new_note.html', urlencodedParser, (req, res) => {
-  if (req.body.noteName && req.body.noteText) {
     db.run('INSERT INTO notes (noteName,noteText,likes,tagsCount) VALUES (?,?,?,?)', req.body.noteName, req.body.noteText,0,0);
     res.send('Ваша заметка ' + req.body.noteName + ' успешно сохранена<br><a href="/my_notes.html">вернуться к моим заметкам</a>');
-  }
-  else
-    res.redirect('/new_note.html');
 });
 
-
+app.post('/login.html',urlencodedParser,(req,res) => {
+  db.each('SELECT userName,password FROM users', (err,user) => {
+    if (err) res.status(404).send('что то пошло не так...');
+    else {
+    if ((req.body.userName===user.userName) && (req.body.password===user.password))
+      res.send('Welcome, '+user.userName+'<br><a href="/">на главную</a>');
+    else 
+      res.send('Такого пользователя не существует. <div><a href="/login.html">назад</a></div><div><a href="/">на главную</a></div>');
+    }
+  });
+});
 /*
 app.get('/new_page', (req,res) => {
   let data=[];
